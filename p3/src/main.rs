5063e8eb36510e0ed203eab4116edd1ebb5d9fee
Clayton Knittel <cknittel@ip-172-31-40-145.us-east-2.compute.internal>
2023-01-28 07:04:40 +0000

    p3 p2

--- p3/src/main.rs
+++ p3/src/main.rs
@@ -7,28 +7,35 @@ fn char_to_idx(c: u32) -> u32 {
   h1 + offset - 1
 }
 
-fn item_prio(rucksack: &str) -> u32 {
-  let mut s1 = 0u64;
-  let mut s2 = 0u64;
-
-  for c in rucksack[..rucksack.len() / 2].chars() {
+fn rucksack_mask(rucksack: &str) -> u64 {
+  rucksack.chars().fold(0u64, |mask, c| {
     let offset = char_to_idx(c as u32);
-    s1 |= 1u64 << offset;
-  }
-  for c in rucksack[rucksack.len() / 2..].chars() {
-    let offset = char_to_idx(c as u32);
-    s2 |= 1u64 << offset;
-  }
+    mask | (1u64 << offset)
+  })
+}
 
-  let intersect = s1 & s2;
+fn item_prio(r1: &str, r2: &str, r3: &str) -> u32 {
+  let s1 = rucksack_mask(r1);
+  let s2 = rucksack_mask(r2);
+  let s3 = rucksack_mask(r3);
+
+  let intersect = s1 & s2 & s3;
   intersect.trailing_zeros() + 1
 }
 
 fn main() -> Result<(), std::io::Error> {
-  let mut contents = utils::get_input()?;
+  let contents: Vec<String> = utils::get_input()?
+    .into_iter()
+    .collect::<Result<Vec<String>, std::io::Error>>()?;
 
-  let prio_sum = contents.try_fold(0u32, |prio_sum, rucksack| {
-    Ok::<u32, std::io::Error>(prio_sum + item_prio(&rucksack?))
+  let mut iter = itertools::izip!(
+    contents.iter().step_by(3),
+    contents.iter().skip(1).step_by(3),
+    contents.iter().skip(2).step_by(3)
+  );
+
+  let prio_sum = iter.try_fold(0u32, |prio_sum, (r1, r2, r3)| {
+    Ok::<u32, std::io::Error>(prio_sum + item_prio(&r1, &r2, &r3))
   })?;
 
   println!("Sum of item priorities: {}", prio_sum);
