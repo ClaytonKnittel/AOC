6b7be2082b4a9136cab78daacf8302c4a0a7c636
Clayton Knittel <cknittel@ip-172-31-40-145.us-east-2.compute.internal>
2023-01-29 06:31:08 +0000

    p1/p2 cmd line args in utils

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,6 +1,26 @@
+use clap::Parser;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  /// Which part of the problem to run. Default is 1.
+  #[arg(short, long, default_value_t = 1)]
+  part: u32,
+}
+
+pub fn is_p1() -> bool {
+  lazy_static! {
+    static ref ARGS: Args = Args::parse();
+  }
+
+  ARGS.part == 1
+}
 
 pub fn get_input() -> Result<io::Lines<io::BufReader<File>>, std::io::Error> {
   let file = File::open(&Path::new("input.txt"))?;
