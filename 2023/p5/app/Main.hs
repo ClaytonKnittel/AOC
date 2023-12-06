e7f62d6aefa9ccd234dc7052d344ed87fdd74e8d
Clayton Knittel <cknit1999@gmail.com>
2023-12-06 01:44:43 -0800

    q5 p2

--- 2023/p5/app/Main.hs
+++ 2023/p5/app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
-import Data.Set (Set, empty, insert, lookupLE, toList)
+import Data.Set (Set, empty, insert, lookupGT, lookupLE, toList)
+import Data.Tree (flatten)
+
+data Seed = Seed {seed, seedLen :: Integer} deriving (Show)
 
 data IdRange = IdRange {src, dst, len :: Integer}
 
@@ -28,6 +31,31 @@ lookupId id (IdMap set) =
             else id
         Nothing -> id
 
+-- Computes the number of id's you'd need to increment `id` by before it would
+-- not map to the same range of the dst. `Nothing` means id can increment by an
+-- infinite amount in the same range in dst.
+distToDiff :: Integer -> IdMap -> Maybe Integer
+distToDiff id (IdMap set) =
+  let resLE = lookupLE IdRange {src = id, dst = 0, len = 0} set
+      resGT = lookupGT IdRange {src = id, dst = 0, len = 0} set
+   in case (resLE, resGT) of
+        (Nothing, Nothing) -> Nothing
+        (Nothing, Just IdRange {src = src}) -> Just (src - id)
+        (Just IdRange {src = src, len = len}, Nothing) ->
+          if id >= src + len
+            then Nothing
+            else Just (src + len - id)
+        (Just IdRange {src = src, len = len}, Just IdRange {src = src2}) ->
+          if id >= src + len
+            then Just (src2 - id)
+            else Just (src + len - id)
+
+minMaybe :: Maybe Integer -> Maybe Integer -> Maybe Integer
+minMaybe Nothing Nothing = Nothing
+minMaybe Nothing b = b
+minMaybe a Nothing = a
+minMaybe (Just a) (Just b) = Just (min a b)
+
 insertId :: IdRange -> IdMap -> IdMap
 insertId idRange (IdMap set) = IdMap (insert idRange set)
 
@@ -40,10 +68,33 @@ seedToLocations id (idMap : idMaps) =
   let mappedId = lookupId id idMap
    in (id : seedToLocations mappedId idMaps)
 
+seedToLocationWithDiff :: Integer -> [IdMap] -> (Integer, Maybe Integer)
+seedToLocationWithDiff id [] = (id, Nothing)
+seedToLocationWithDiff id (idMap : idMaps) =
+  let mappedId = lookupId id idMap
+      diff = distToDiff id idMap
+      (finalId, remDiff) = seedToLocationWithDiff mappedId idMaps
+   in (finalId, minMaybe diff remDiff)
+
+minSeedRange :: Seed -> [IdMap] -> Integer
+minSeedRange (Seed {seed = seed, seedLen = len}) idMaps =
+  let (location, diff) = seedToLocationWithDiff seed idMaps
+   in case diff of
+        Just diff ->
+          if diff >= len
+            then location
+            else min location (minSeedRange (Seed {seed = seed + diff, seedLen = len - diff}) idMaps)
+        Nothing -> location
+
 parseSeeds :: [String] -> [Integer]
 parseSeeds [] = []
 parseSeeds ("seeds:" : rem) = parseSeeds rem
 parseSeeds (seed : rem) = read seed : parseSeeds rem
+
+parseSeeds2 :: [String] -> [Seed]
+parseSeeds2 [] = []
+parseSeeds2 ("seeds:" : rem) = parseSeeds2 rem
+parseSeeds2 (seed : len : rem) = Seed {seed = read seed, seedLen = read len} : parseSeeds2 rem
 
 parseIdRanges :: [String] -> [IdMap]
 parseIdRanges [] = [IdMap empty]
@@ -63,8 +114,15 @@ parseContents contents =
   let (seeds : "" : rem) = lines contents
    in (parseSeeds (words seeds), parseMaps rem)
 
+parseContents2 :: String -> ([Seed], [IdMap])
+parseContents2 contents =
+  let (seeds : "" : rem) = lines contents
+   in (parseSeeds2 (words seeds), parseMaps rem)
+
 main :: IO ()
 main = do
   contents <- readFile "input.txt"
-  let (seeds, idMaps) = parseContents contents
-   in print (minimum (map (`seedToLocation` idMaps) seeds))
+  -- let (seeds, idMaps) = parseContents contents
+  --  in print (minimum (map (`seedToLocation` idMaps) seeds))
+  let (seeds, idMaps) = parseContents2 contents
+   in print (minimum (map (`minSeedRange` idMaps) seeds))
