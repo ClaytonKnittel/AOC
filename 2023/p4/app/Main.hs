57911c5421ddd517a5f3667028c030f23afb72f2
Clayton Knittel <cknit1999@gmail.com>
2023-12-06 00:06:43 -0800

    day4 part 2

--- 2023/p4/app/Main.hs
+++ 2023/p4/app/Main.hs
@@ -7,12 +7,24 @@ data Card = Card {number :: Integer, winning, nums :: Set Integer}
 instance Show Card where
   show Card {number = n, winning = w, nums = nums} = "Card " ++ show n ++ ": " ++ show (toList w) ++ " | " ++ show (toList nums)
 
+cardOverlap :: Card -> Integer
+cardOverlap Card {winning = w, nums = n} = (toInteger . size) (w `intersection` n)
+
+overlapScore :: Integer -> Integer
+overlapScore overlap =
+  case overlap of
+    0 -> 0
+    n -> 2 ^ (n - 1)
+
 cardScore :: Card -> Integer
-cardScore Card {winning = w, nums = n} =
-  let overlap = w `intersection` n
-   in case size overlap of
-        0 -> 0
-        n -> 2 ^ (n - 1)
+cardScore = overlapScore . cardOverlap
+
+cardCountP2 :: [Card] -> [Integer]
+cardCountP2 [] = []
+cardCountP2 (card : cards) =
+  let counts = cardCountP2 cards
+      overlap = (fromIntegral . cardOverlap) card
+   in (1 + sum (take overlap counts) : counts)
 
 parseMyNums :: [String] -> Card
 parseMyNums [] = Card {number = 0, winning = empty, nums = empty}
@@ -33,4 +45,5 @@ parseLine s = case words s of
 main :: IO ()
 main = do
   contents <- readFile "input.txt"
-  print (sum (map (cardScore . parseLine) (lines contents)))
+  -- print (sum (map (cardScore . parseLine) (lines contents)))
+  print ((sum . cardCountP2 . map parseLine) (lines contents))
