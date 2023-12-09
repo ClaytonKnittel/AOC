d0e58a88f809dacf4c5d8e904c9ba69a9fbc8fb1
Clayton Knittel <cknit1999@gmail.com>
2023-12-09 01:23:59 -0800

    q9 p2

--- 2023/p9/app/Main.hs
+++ 2023/p9/app/Main.hs
@@ -18,7 +18,21 @@ predictNext measurements
             measurements
         )
 
+predictPrev :: [Integer] -> Integer
+predictPrev measurements
+  | all (== 0) measurements = 0
+  | otherwise =
+    head measurements
+      - predictPrev
+        ( zipWith
+            (-)
+            (tail measurements)
+            measurements
+        )
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   print (sum (map predictNext (parseInput input)))
+    >> print
+      (sum (map predictPrev (parseInput input)))
