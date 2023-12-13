5f4cd49039baaa66f7589fca0e361df9e2b0f22c
Clayton Knittel <cknit1999@gmail.com>
2023-12-13 00:43:36 -0800

    q13 p2

--- 2023/p13/src/Lib.hs
+++ 2023/p13/src/Lib.hs
@@ -6,6 +6,7 @@ where
 import Data.List (findIndex, inits, tails, transpose)
 import Data.List.Split (splitOn)
 import Data.Maybe (fromMaybe)
+import GHC.Utils.Misc (count)
 
 findMirror :: (Eq a) => [[a]] -> Maybe Integer
 findMirror =
@@ -20,10 +21,28 @@ findMirror =
             (tail $ tails row)
       )
 
+findMirror2 :: (Eq a) => [[a]] -> Maybe Integer
+findMirror2 =
+  fmap ((1 +) . toInteger)
+    . findIndex ((== 1) . sum)
+    . transpose
+    . map
+      ( \row ->
+          zipWith
+            (\l r -> count (uncurry (/=)) $ zip l r)
+            (map reverse $ init $ tail $ inits row)
+            (tail $ tails row)
+      )
+
 mirrorIndex :: (Eq a) => [[a]] -> Integer
 mirrorIndex rows =
   (fromMaybe 0 . findMirror) rows
     + 100 * (fromMaybe 0 . findMirror $ transpose rows)
+
+mirrorIndex2 :: (Eq a) => [[a]] -> Integer
+mirrorIndex2 rows =
+  (fromMaybe 0 . findMirror2) rows
+    + 100 * (fromMaybe 0 . findMirror2 $ transpose rows)
 
 parseInput :: String -> [[String]]
 parseInput input = splitOn [""] (lines input)
@@ -33,3 +52,4 @@ mirror = do
   input <- readFile "input.txt"
   let grids = parseInput input
    in print (sum $ map mirrorIndex grids)
+        <> print (sum $ map mirrorIndex2 grids)
