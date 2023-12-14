23f7d7a91ca351771fcfc57aba47050eebfe82b4
Clayton Knittel <cknit1999@gmail.com>
2023-12-14 01:39:33 -0800

    q14 p2

--- 2023/p14/src/Lib.hs
+++ 2023/p14/src/Lib.hs
@@ -5,10 +5,13 @@ module Lib
   )
 where
 
-import Data.List (genericLength, transpose)
+import Data.Bool (bool)
+import Data.List (find, genericLength, transpose)
+import Data.Map (Map, assocs, empty, insert, lookup)
 import Data.Maybe (fromMaybe)
+import Prelude hiding (lookup)
 
-data Tile = O | S | Empty
+data Tile = O | S | Empty deriving (Eq, Ord)
 
 instance Show Tile where
   show = \case
@@ -41,6 +44,60 @@ rockTiltLoadCol = tiltLoad <*> genericLength
 rockTiltLoad :: [[Tile]] -> Integer
 rockTiltLoad = sum . map rockTiltLoadCol . transpose
 
+rockTiltLoadColCnst :: [Tile] -> Integer
+rockTiltLoadColCnst =
+  sum
+    . flip
+      (zipWith $ bool (const 0) id . (== O))
+      [1 ..]
+    . reverse
+
+-- rockTiltLoadColCnst = sum . zipWith (\x t -> bool 0 x (t == O)) [1 ..]
+
+rockTiltLoadCnst :: [[Tile]] -> Integer
+rockTiltLoadCnst = sum . map rockTiltLoadColCnst
+
+-- Slides all O rocks toward the beginning of the list
+-- without passing any S rocks.
+shiftRocks :: [Tile] -> [Tile]
+shiftRocks = flip shiftR []
+  where
+    shiftR [] = ([] ++)
+    shiftR (tile : tiles) = case tile of
+      O -> (tile :) . shiftR tiles
+      S -> (++ S : shiftRocks tiles)
+      Empty -> shiftR tiles . ([Empty] ++)
+
+cwRotateGrid :: [[a]] -> [[a]]
+cwRotateGrid = map reverse . transpose
+
+ccwRotateGrid :: [[a]] -> [[a]]
+ccwRotateGrid = transpose . map reverse
+
+runCycle :: [[Tile]] -> [[Tile]]
+runCycle = (!! 4) . iterate (cwRotateGrid . map shiftRocks)
+
+stateAfterNSteps :: (Ord a) => (a -> a) -> a -> Integer -> a
+stateAfterNSteps = stepN empty
+  where
+    stepN :: (Ord a) => Map a Integer -> (a -> a) -> a -> Integer -> a
+    stepN _ _ state 0 = state
+    stepN m fn state steps' =
+      maybe
+        ( stepN
+            (insert state steps' m)
+            fn
+            (fn state)
+            (steps' - 1)
+        )
+        ( \x ->
+            maybe
+              state
+              fst
+              $ find ((== x - (steps' `mod` (x - steps'))) . snd) (assocs m)
+        )
+        (lookup state m)
+
 parseInput :: String -> Maybe [[Tile]]
 parseInput = mapM (mapM parseTile) . lines
 
@@ -51,3 +108,10 @@ rockTilt =
     let rocks = fromMaybe [[]] $ parseInput input
      in printTiles rocks
           <> print (rockTiltLoad rocks)
+          <> print
+            ( rockTiltLoadCnst $
+                stateAfterNSteps
+                  runCycle
+                  (ccwRotateGrid rocks)
+                  1000000000
+            )
