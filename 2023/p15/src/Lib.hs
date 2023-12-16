6fa27c891645eef4d7f7e1228d0611a2124eea21
Clayton Knittel <cknit1999@gmail.com>
2023-12-16 00:52:27 -0800

    partial q15 p2

--- 2023/p15/src/Lib.hs
+++ 2023/p15/src/Lib.hs
@@ -1,16 +1,62 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Lib
   ( addSteps,
   )
 where
 
+import Data.Bool (bool)
 import Data.Char (ord)
 import Data.List.Split (splitOn)
+import Data.Map (Map, assocs, empty, findWithDefault, insert)
+import Prelude hiding (lookup)
 
 hash :: Char -> Integer -> Integer
 hash = ((((`mod` 256) . (* 17)) .) . (+)) . (toInteger . ord)
+
+data Op = Remove String Integer | Remap Integer String Integer
+
+parseOp :: String -> Op
+parseOp opStr = parse opStr "" 0
+  where
+    parse :: String -> String -> Integer -> Op
+    parse ['-'] = Remove
+    parse ('=' : focalLength) = Remap (read focalLength)
+    parse (c : cs) = (. hash c) . parse cs . (++ [c])
+
+applyOp :: Op -> Map Integer [(String, Integer)] -> Map Integer [(String, Integer)]
+applyOp (Remove str idx) m =
+  insert
+    idx
+    ( filter
+        ((/= str) . fst)
+        (findWithDefault [] idx m)
+    )
+    m
+applyOp (Remap focalLength str idx) m =
+  insert
+    idx
+    ( filter
+        ((/= str) . fst)
+        (findWithDefault [] idx m)
+        ++ [(str, focalLength)]
+    )
+    m
+
+focusingPower :: Map Integer [(String, Integer)] -> Integer
+focusingPower =
+  sum
+    . map
+      ( uncurry $
+          (. (sum . zipWith (flip $ (*) . snd) [1 ..]))
+            . (*)
+            . (+ 1)
+      )
+    . assocs
 
 addSteps :: IO ()
 addSteps =
   do
     input <- readFile "input.txt"
     print $ sum $ map (foldl (flip hash) 0) $ splitOn "," $ filter (/= '\n') input
+    print $ foldl (flip $ applyOp . parseOp) empty $ splitOn "," $ filter (/= '\n') input
