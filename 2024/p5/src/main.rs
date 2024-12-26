199b5451a0a23ff1dd6daee18dee3b05bd9ce0a0
Clayton Knittel <cknit1999@gmail.com>
2024-12-25 23:33:12 -0800

    p5 p2

--- 2024/p5/src/main.rs
+++ 2024/p5/src/main.rs
@@ -60,6 +60,55 @@ impl RuleGraph {
 
     true
   }
+
+  fn reorder(&self, print: &Vec<u32>) -> Vec<u32> {
+    let mut new_print = Vec::new();
+    let mut deps: HashMap<u32, (HashSet<u32>, Vec<u32>)> = print
+      .iter()
+      .map(|&page| (page, (HashSet::new(), Vec::new())))
+      .collect();
+    let mut no_deps: HashSet<_> = print.iter().map(u32::clone).collect();
+
+    for &page in print {
+      for pred in self.predecessors(page) {
+        if !print.contains(&pred) {
+          continue;
+        }
+
+        let page_deps = deps.get_mut(&page).unwrap();
+        page_deps.0.insert(pred);
+        no_deps.remove(&page);
+
+        let pred_deps = deps.get_mut(&pred).unwrap();
+        pred_deps.1.push(page);
+      }
+    }
+
+    let mut first_elem = *no_deps.iter().next().unwrap();
+    no_deps.remove(&first_elem);
+    loop {
+      new_print.push(first_elem);
+
+      for dep in deps.get(&first_elem).unwrap().1.clone() {
+        let dep_deps = deps.get_mut(&dep).unwrap();
+        dep_deps.0.remove(&first_elem);
+        if dep_deps.0.is_empty() {
+          no_deps.insert(dep);
+        }
+      }
+
+      if let Some(&next_elem) = no_deps.iter().next() {
+        no_deps.remove(&next_elem);
+        first_elem = next_elem;
+      } else {
+        break;
+      }
+    }
+
+    assert!(new_print.len() == print.len());
+
+    new_print
+  }
 }
 
 fn parse_input(input_file: &str) -> AocResult<(RuleGraph, Vec<Vec<u32>>)> {
@@ -107,6 +156,19 @@ fn main() -> AocResult {
     .sum::<u32>();
 
   println!("Valid prints: {valid_prints}");
+
+  let reordered_prints = prints
+    .iter()
+    .flat_map(|print| {
+      if !rule_graph.is_valid_print(print) {
+        Some(rule_graph.reorder(print)[print.len() / 2])
+      } else {
+        None
+      }
+    })
+    .sum::<u32>();
+
+  println!("Reordered prints: {reordered_prints}");
 
   Ok(())
 }
