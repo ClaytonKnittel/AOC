613c07b05d24baf25b2465ecb3046699f17036d8
Clayton Knittel <cknit1999@gmail.com>
2024-12-27 23:49:02 -0800

    p14 p2

--- 2024/p14/src/main.rs
+++ 2024/p14/src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, str::FromStr};
+use std::{borrow::Borrow, collections::HashSet, error::Error, str::FromStr};
 
 use once_cell::sync::Lazy;
 use regex::Regex;
@@ -69,14 +69,15 @@ trait T: Sized {}
 
 impl T for Robot {}
 
-fn quadrant_product<I>(robots: I, steps: u64, width: usize, height: usize) -> u64
+fn quadrant_product<I, R>(robots: I, steps: u64, width: usize, height: usize) -> u64
 where
-  I: IntoIterator<Item = Robot>,
+  I: IntoIterator<Item = R>,
+  R: Borrow<Robot>,
 {
   robots
     .into_iter()
     .fold(vec![0; 4], |mut quadrants, robot| {
-      let final_pos = robot.find_position_after(steps, width, height);
+      let final_pos = robot.borrow().find_position_after(steps, width, height);
       if let Some(q_idx) = quadrant(final_pos, width, height) {
         quadrants[q_idx] += 1;
       }
@@ -86,6 +87,60 @@ where
     .product()
 }
 
+#[allow(dead_code)]
+fn bounding_box_area<I, R>(robots: I, steps: u64, width: usize, height: usize) -> isize
+where
+  I: IntoIterator<Item = R>,
+  R: Borrow<Robot>,
+{
+  let (min, max) = robots.into_iter().fold(
+    (
+      Pos {
+        row: height as isize - 1,
+        col: width as isize - 1,
+      },
+      Pos { row: 0, col: 0 },
+    ),
+    |(min, max), robot| {
+      let final_pos = robot.borrow().find_position_after(steps, width, height);
+
+      (
+        Pos {
+          row: min.row.min(final_pos.row),
+          col: min.col.min(final_pos.col),
+        },
+        Pos {
+          row: max.row.max(final_pos.row),
+          col: max.col.max(final_pos.col),
+        },
+      )
+    },
+  );
+
+  (max.row - min.row + 1) * (max.col - min.col + 1)
+}
+
+fn tightness<I, R>(robots: I, steps: u64, width: usize, height: usize) -> usize
+where
+  I: IntoIterator<Item = R>,
+  R: Borrow<Robot>,
+{
+  let positions = robots
+    .into_iter()
+    .map(|robot| robot.borrow().find_position_after(steps, width, height))
+    .collect::<HashSet<_>>();
+
+  positions
+    .iter()
+    .filter(|&&pos| {
+      positions.contains(&(pos + Diff { dr: 0, dc: 1 }))
+        || positions.contains(&(pos + Diff { dr: 0, dc: -1 }))
+        || positions.contains(&(pos + Diff { dr: 1, dc: 0 }))
+        || positions.contains(&(pos + Diff { dr: -1, dc: 0 }))
+    })
+    .count()
+}
+
 fn main() -> AocResult {
   const INPUT_FILE: &str = "input.txt";
   const WIDTH: usize = 101;
@@ -93,8 +148,34 @@ fn main() -> AocResult {
 
   let robots = list_of_objects(INPUT_FILE)?;
 
-  let product = quadrant_product(robots, 100, WIDTH, HEIGHT);
+  let product = quadrant_product(robots.iter(), 100, WIDTH, HEIGHT);
   println!("Product of quadrants: {product}");
+
+  let mut max_tightness = 0;
+  let mut min_steps = 0;
+  for steps in 0..100000 {
+    let p = tightness(robots.iter(), steps, WIDTH, HEIGHT);
+
+    if p > max_tightness {
+      max_tightness = p;
+      min_steps = steps;
+    }
+  }
+
+  println!("Max tightness: {max_tightness} after {min_steps} steps");
+
+  let mut grid = [[0; WIDTH]; HEIGHT];
+  for robot in robots.iter() {
+    let pos = robot.find_position_after(min_steps, WIDTH, HEIGHT);
+    grid[pos.row as usize][pos.col as usize] += 1;
+  }
+  println!("Steps: {min_steps}");
+  for row in grid {
+    for col in row {
+      print!("{col}");
+    }
+    println!();
+  }
 
   Ok(())
 }
