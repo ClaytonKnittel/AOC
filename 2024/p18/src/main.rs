6d4b7a7a1e9e6d9d79e24bf06a5690be9a726690
Clayton Knittel <cknit1999@gmail.com>
2024-12-29 10:44:16 -0800

    p18 p2

--- 2024/p18/src/main.rs
+++ 2024/p18/src/main.rs
@@ -40,6 +40,12 @@ fn main() -> AocResult {
   const HEIGHT: usize = 71;
   const FALLEN_BYTES: usize = 1024;
 
+  const START: Pos = Pos { col: 0, row: 0 };
+  const END: Pos = Pos {
+    col: WIDTH as isize - 1,
+    row: HEIGHT as isize - 1,
+  };
+
   let mut input_str = String::new();
   BufReader::new(File::open(INPUT_FILE)?).read_to_string(&mut input_str)?;
   let byte_coords: Vec<_> = input_str
@@ -60,15 +66,25 @@ fn main() -> AocResult {
     },
   );
 
-  let dist_to_end = do_dijkstra(
-    &grid,
-    Pos { col: 0, row: 0 },
-    Pos {
-      col: WIDTH as isize - 1,
-      row: HEIGHT as isize - 1,
-    },
-  );
+  let dist_to_end = do_dijkstra(&grid, START, END);
   println!("Distance to end: {dist_to_end}");
+
+  let coord_of_blocker = byte_coords
+    .iter()
+    .cloned()
+    .skip(FALLEN_BYTES)
+    .scan(grid, |grid, (col, row)| {
+      let pos = Pos { col, row };
+      grid[pos] = b'#';
+      Some((do_dijkstra(grid, START, END) == u64::MAX).then_some(pos))
+    })
+    .flatten()
+    .next()
+    .unwrap();
+  println!(
+    "Coordinate of blocker: {},{}",
+    coord_of_blocker.col, coord_of_blocker.row
+  );
 
   Ok(())
 }
