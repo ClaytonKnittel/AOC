057b8bf7682a93d86e8260d4c877883fee8b5ad2
Clayton Knittel <cknit1999@gmail.com>
2024-12-29 14:07:25 -0800

    p19 p2

--- 2024/p19/src/main.rs
+++ 2024/p19/src/main.rs
@@ -17,6 +17,26 @@ fn possible(pattern: &[u8], towels: &[&[u8]]) -> bool {
   })[0]
 }
 
+fn ways(pattern: &[u8], towels: &[&[u8]]) -> u64 {
+  let mut m = vec![0; pattern.len() + 1];
+  m[pattern.len()] = 1;
+
+  (0..pattern.len()).rev().fold(m, |mut m, from_idx| {
+    let to_cover = &pattern[from_idx..];
+    m[from_idx] = towels
+      .iter()
+      .map(|&towel| {
+        if towel.len() <= to_cover.len() && towel == &to_cover[..towel.len()] {
+          m[from_idx + towel.len()]
+        } else {
+          0
+        }
+      })
+      .sum();
+    m
+  })[0]
+}
+
 fn main() -> AocResult {
   const INPUT_FILE: &str = "input.txt";
   let contents = read_to_string(INPUT_FILE)?;
@@ -36,6 +56,9 @@ fn main() -> AocResult {
     .filter(|pattern| possible(pattern, &towels))
     .count();
   println!("Num possible patterns: {num_possible_patterns}");
+
+  let sum_possible_ways: u64 = patterns.iter().map(|pattern| ways(pattern, &towels)).sum();
+  println!("Sum of possible ways: {sum_possible_ways}");
 
   Ok(())
 }
