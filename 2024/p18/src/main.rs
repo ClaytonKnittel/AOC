c372f5ca6664ac595c269f16ee06df921174ee0d
Clayton Knittel <cknit1999@gmail.com>
2024-12-29 10:26:36 -0800

    p18 p1

---
+++ 2024/p18/src/main.rs
@@ -0,0 +1,74 @@
+use std::{
+  collections::{BTreeSet, HashSet},
+  fs::File,
+  io::{BufReader, Read},
+};
+
+use util::{
+  error::{AocError, AocResult},
+  grid::{Grid, Pos},
+};
+
+fn do_dijkstra(maze: &Grid, start: Pos, end: Pos) -> u64 {
+  let mut scored_tiles = HashSet::<Pos>::new();
+  let mut to_explore = BTreeSet::new();
+  to_explore.insert((0, start));
+
+  while let Some((distance, pos)) = to_explore.pop_first() {
+    if pos == end {
+      return distance;
+    }
+
+    if !scored_tiles.insert(pos) {
+      continue;
+    }
+
+    to_explore.extend(
+      maze
+        .orthogonal_neighbors(pos)
+        .filter(|pos| maze[pos] == b'.')
+        .map(|neighbor_pos: Pos| (distance + 1, neighbor_pos)),
+    );
+  }
+
+  u64::MAX
+}
+
+fn main() -> AocResult {
+  const INPUT_FILE: &str = "input.txt";
+  const WIDTH: usize = 71;
+  const HEIGHT: usize = 71;
+  const FALLEN_BYTES: usize = 1024;
+
+  let mut input_str = String::new();
+  BufReader::new(File::open(INPUT_FILE)?).read_to_string(&mut input_str)?;
+  let byte_coords: Vec<_> = input_str
+    .lines()
+    .map(|line| {
+      let (col, row) = line
+        .split_once(',')
+        .ok_or_else(|| AocError::Parse(format!("Line {line} does not contain a ','")))?;
+      Ok((col.parse()?, row.parse()?))
+    })
+    .collect::<AocResult<_>>()?;
+
+  let grid = byte_coords.iter().cloned().take(FALLEN_BYTES).fold(
+    Grid::new(vec![vec![b'.'; WIDTH]; HEIGHT]),
+    |mut grid, (col, row)| {
+      grid[Pos { col, row }] = b'#';
+      grid
+    },
+  );
+
+  let dist_to_end = do_dijkstra(
+    &grid,
+    Pos { col: 0, row: 0 },
+    Pos {
+      col: WIDTH as isize - 1,
+      row: HEIGHT as isize - 1,
+    },
+  );
+  println!("Distance to end: {dist_to_end}");
+
+  Ok(())
+}
