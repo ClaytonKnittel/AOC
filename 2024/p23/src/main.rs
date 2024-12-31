7473f96df23deaffa8a01261fa9a42e92582ef7d
Clayton Knittel <cknit1999@gmail.com>
2024-12-31 09:21:08 -0800

    p23 p1

---
+++ 2024/p23/src/main.rs
@@ -0,0 +1,124 @@
+use std::{
+  collections::{hash_map::Entry, HashMap, HashSet},
+  error::Error,
+  fmt::{self, Display, Formatter},
+  fs::read_to_string,
+  hash::Hash,
+  str::FromStr,
+};
+
+use itertools::Itertools;
+use util::error::{AocError, AocResult};
+
+#[derive(Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+struct Name {
+  name: u32,
+}
+
+impl Name {
+  fn first_letter(&self) -> u8 {
+    (self.name / 26) as u8 + b'a'
+  }
+
+  fn second_letter(&self) -> u8 {
+    (self.name % 26) as u8 + b'a'
+  }
+}
+
+impl Display for Name {
+  fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    write!(
+      f,
+      "{}{}",
+      self.first_letter() as char,
+      self.second_letter() as char
+    )
+  }
+}
+
+impl FromStr for Name {
+  type Err = Box<dyn Error>;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    if s.len() != 2 {
+      return Err(AocError::Parse(format!("\"{s}\" is not a 2-letter name")).into());
+    }
+
+    let d1 = s.as_bytes()[0];
+    let d2 = s.as_bytes()[1];
+    if !d1.is_ascii_alphabetic() || !d2.is_ascii_alphabetic() {
+      return Err(AocError::Parse(format!("\"{s}\" is not a 2-letter alphabetic name")).into());
+    }
+
+    Ok(Self {
+      name: (d1 - b'a') as u32 * 26 + (d2 - b'a') as u32,
+    })
+  }
+}
+
+fn interconnected_sets_of_3_with_t(connections: &HashMap<Name, HashSet<Name>>) -> u64 {
+  connections
+    .iter()
+    .flat_map(|(&k, k_connections)| {
+      k_connections
+        .iter()
+        .combinations(2)
+        .filter(move |combo| {
+          let conn1 = combo[0];
+          let conn2 = combo[1];
+          connections
+            .get(conn1)
+            .is_some_and(|conn| conn.contains(&k) && conn.contains(conn2))
+            && connections
+              .get(conn2)
+              .is_some_and(|conn| conn.contains(&k) && conn.contains(conn1))
+        })
+        .map(move |combo| [k, *combo[0], *combo[1]])
+    })
+    .map(|mut elems| {
+      elems.sort();
+      elems
+    })
+    .filter(|elems| elems.iter().any(|name| name.first_letter() == b't'))
+    .count() as u64
+    / 3
+}
+
+fn make_connection<T>(map: &mut HashMap<T, HashSet<T>>, t1: T, t2: T)
+where
+  T: Clone + Hash + PartialEq + Eq,
+{
+  let mut insert = |t1: T, t2: T| match map.entry(t1) {
+    Entry::Occupied(mut entry) => {
+      entry.get_mut().insert(t2);
+    }
+    Entry::Vacant(entry) => {
+      entry.insert([t2].into());
+    }
+  };
+
+  insert(t1.clone(), t2.clone());
+  insert(t2, t1);
+}
+
+fn main() -> AocResult {
+  const INPUT_FILE: &str = "input.txt";
+  let connections = read_to_string(INPUT_FILE)?
+    .lines()
+    .map(|line| -> AocResult<_> {
+      let (from_name, to_name) = line
+        .split_once('-')
+        .ok_or_else(|| AocError::Parse(format!("Line \"{line}\" does not contain a '-'")))?;
+      Ok((from_name.parse::<Name>()?, to_name.parse::<Name>()?))
+    })
+    .try_fold(HashMap::new(), |mut map, names| -> AocResult<_> {
+      let (name1, name2) = names?;
+      make_connection(&mut map, name1, name2);
+      Ok(map)
+    })?;
+
+  let num_with_t = interconnected_sets_of_3_with_t(&connections);
+  println!("Sets of 3 with t-name: {num_with_t}");
+
+  Ok(())
+}
