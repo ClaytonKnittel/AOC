4e598081cbb2b7d6c186384e2afc6acf6be3c8e7
Clayton Knittel <cknit1999@gmail.com>
2024-12-31 15:18:52 -0800

    p24 p1

--- 2024/p24/src/main.rs
+++ 2024/p24/src/main.rs
@@ -1,12 +1,16 @@
 use std::{
+  array,
+  collections::HashMap,
   error::Error,
   fmt::{self, Display, Formatter},
   fs::read_to_string,
+  iter::Flatten,
   ops::{BitAnd, BitOr, BitXor},
   str::FromStr,
 };
 
 use util::{
+  algorithm::{TopologicalOrd, TopologicalSort},
   error::{AocError, AocResult},
   iter_util::iter_to_slice,
 };
@@ -14,6 +18,12 @@ use util::{
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 struct Bool {
   val: bool,
+}
+
+impl From<bool> for Bool {
+  fn from(value: bool) -> Self {
+    Bool { val: value }
+  }
 }
 
 impl BitAnd<Self> for Bool {
@@ -67,6 +77,32 @@ impl Display for Bool {
 enum Statement<T> {
   Declaration(Declaration<T>),
   Condition(Condition<T>),
+}
+
+impl<T> TopologicalOrd<T> for Statement<T>
+where
+  T: Clone,
+{
+  type DependsOnIter = Flatten<array::IntoIter<Option<T>, 2>>;
+
+  fn key(&self) -> T {
+    match self {
+      Self::Declaration(declaration) => declaration.variable.clone(),
+      Self::Condition(condition) => condition.result.clone(),
+    }
+  }
+
+  fn depends_on(&self) -> Self::DependsOnIter {
+    match self {
+      Self::Declaration(_) => [None, None],
+      Self::Condition(condition) => [
+        Some(condition.op.lhs().clone()),
+        Some(condition.op.rhs().clone()),
+      ],
+    }
+    .into_iter()
+    .flatten()
+  }
 }
 
 impl<T: Display> Display for Statement<T> {
@@ -155,6 +191,24 @@ enum ConditionOp<T> {
   Xor { l: T, r: T },
 }
 
+impl<T> ConditionOp<T> {
+  fn lhs(&self) -> &T {
+    match self {
+      Self::And { l, r: _ } => l,
+      Self::Or { l, r: _ } => l,
+      Self::Xor { l, r: _ } => l,
+    }
+  }
+
+  fn rhs(&self) -> &T {
+    match self {
+      Self::And { l: _, r } => r,
+      Self::Or { l: _, r } => r,
+      Self::Xor { l: _, r } => r,
+    }
+  }
+}
+
 impl<T> ConditionOp<T>
 where
   T: BitAnd<T, Output = T> + BitOr<T, Output = T> + BitXor<T, Output = T> + Clone,
@@ -178,6 +232,97 @@ impl<T: Display> Display for ConditionOp<T> {
   }
 }
 
+struct NumberSolver {
+  statements: Vec<Statement<usize>>,
+  number_bit_idx: Vec<usize>,
+}
+
+impl NumberSolver {
+  fn new(
+    bindings: impl Iterator<Item = Declaration<String>>,
+    conditions: impl Iterator<Item = Condition<String>>,
+  ) -> AocResult<Self> {
+    let sorted = TopologicalSort::new(
+      bindings
+        .map(Statement::Declaration)
+        .chain(conditions.map(Statement::Condition)),
+    )
+    .pop_all()?;
+
+    let mut name_map = HashMap::new();
+    let statements = sorted
+      .into_iter()
+      .map(|statement| match statement {
+        Statement::Declaration(Declaration { variable, val }) => {
+          let var_idx = name_map.len();
+          name_map.insert(variable, var_idx);
+          Statement::Declaration(Declaration {
+            variable: var_idx,
+            val,
+          })
+        }
+        Statement::Condition(Condition { op, result }) => {
+          let &l = name_map.get(op.lhs()).unwrap();
+          let &r = name_map.get(op.rhs()).unwrap();
+
+          let result_idx = name_map.len();
+          name_map.insert(result, result_idx);
+
+          Statement::Condition(Condition {
+            result: result_idx,
+            op: match op {
+              ConditionOp::And { l: _, r: _ } => ConditionOp::And { l, r },
+              ConditionOp::Or { l: _, r: _ } => ConditionOp::Or { l, r },
+              ConditionOp::Xor { l: _, r: _ } => ConditionOp::Xor { l, r },
+            },
+          })
+        }
+      })
+      .collect();
+
+    let mut number_bit_idx: Vec<_> = (0..)
+      .map(|idx| format!("z{idx:#02}"))
+      .map_while(|z_var| name_map.get(&z_var).cloned())
+      .collect();
+    number_bit_idx.reverse();
+
+    Ok(Self {
+      statements,
+      number_bit_idx,
+    })
+  }
+
+  fn num_variables(&self) -> usize {
+    self.statements.len()
+  }
+
+  fn solve(&self) -> u64 {
+    let mut bindings: Vec<Bool> = vec![false.into(); self.num_variables()];
+
+    for statement in self.statements.iter() {
+      match statement {
+        Statement::Declaration(Declaration { variable, val }) => {
+          bindings[*variable] = *val;
+        }
+        Statement::Condition(Condition { op, result }) => {
+          let l = bindings[*op.lhs()];
+          let r = bindings[*op.rhs()];
+          bindings[*result] = match op {
+            ConditionOp::And { l: _, r: _ } => ConditionOp::And { l, r },
+            ConditionOp::Or { l: _, r: _ } => ConditionOp::Or { l, r },
+            ConditionOp::Xor { l: _, r: _ } => ConditionOp::Xor { l, r },
+          }
+          .compute();
+        }
+      }
+    }
+
+    self.number_bit_idx.iter().fold(0, |v, &idx| {
+      (v << 1) + if bindings[idx].val { 1 } else { 0 }
+    })
+  }
+}
+
 fn main() -> AocResult {
   const INPUT_FILE: &str = "input.txt";
   let contents = read_to_string(INPUT_FILE)?;
@@ -194,12 +339,8 @@ fn main() -> AocResult {
     .map(|line| line.parse())
     .collect::<AocResult<_>>()?;
 
-  for binding in bindings.iter() {
-    println!("{binding}");
-  }
-  for condition in conditions.iter() {
-    println!("{condition}");
-  }
+  let result = NumberSolver::new(bindings.into_iter(), conditions.into_iter())?.solve();
+  println!("z result: {result}");
 
   Ok(())
 }
